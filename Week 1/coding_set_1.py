c6ab05103b8ce0dfc39b31977ab724e8ccbc791c
Zowie Beha <113861530+1zzowiebeha@users.noreply.github.com>
2024-08-23 14:04:58 -0400

    Add setup for Exercise 3
    Squash previous commits normally

--- Week 1/coding_set_1.py
+++ Week 1/coding_set_1.py
@@ -1,3 +1,9 @@
-"""Program for Exercise A"""
+"""Program for Exercise A, Exercise 3, and ..."""
 
+# Exercise A:
+# I experienced no problems running this line of code.
 print('This is a test of running Python code within a notebook.')
+
+# Exercise 3:
+# ...
+
