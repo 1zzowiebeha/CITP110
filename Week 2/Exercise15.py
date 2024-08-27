a7dbce9ed72fa05e05e2a749f47bf479df52375d
Zowie Beha <113861530+1zzowiebeha@users.noreply.github.com>
2024-08-27 12:48:38 -0400

    Create boilerplate for exercise 15

---
+++ Week 2/Exercise15.py
@@ -0,0 +1,49 @@
+"""A program which reproduces the images depicted in
+Starting Out with Python by Tony Gaddis Haywood,
+Figure 2-40 via the turtle graphics library."""
+
+import turtle
+
+
+def shape1():
+    """Draw shape 1 from Figure 2-40."""
+    pass
+
+
+def shape2():
+    """Draw shape 2 from Figure 2-40."""
+    pass
+
+
+def shape3():
+    """Draw shape 3 from Figure 2-40."""
+    pass
+
+    
+def shape4():
+    """Draw shape 4 from Figure 2-40."""
+    pass
+    
+    
+def shape5():
+    """Draw shape 5 from Figure 2-40."""
+    pass
+    
+    
+def shape6():
+    """Draw shape 6 from Figure 2-40."""
+    pass
+    
+    
+if __name__ == "__main__":
+    # Draw each shape.
+    shape1()
+    shape2()
+    shape3()
+    shape4()
+    shape5()
+    shape6()
+    
+    # Prevent the window from closing once
+    #   images have been drawn.
+    turtle.done()
