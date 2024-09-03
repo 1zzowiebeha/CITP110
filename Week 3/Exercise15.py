1e5d72f03a9d221e597369e7c0c08d523a37094d
Zowie Beha <113861530+1zzowiebeha@users.noreply.github.com>
2024-09-03 09:41:59 -0400

    Reuse zero strip function
    Here I make the output look nice via my
    zero stripper, which strips unneeded zeros
    from a float to turn the numeric into an int.
    
    I created this function and re-used it from
    last week's project.

--- Week 3/Exercise15.py
+++ Week 3/Exercise15.py
@@ -1,6 +1,30 @@
 """Code for Exercise 15 from the textbook."""
 
-
+# This function uses type hints, specifically union types.
+def strip_unwanted_zeros(numeric: float | int) -> str:
+    r"""Remove trailing 0s from a float to turn it into an int representation.
+    Removes all captured characters in ^(?:\d+)(\.0+)$ regex.
+    
+    We convert to a string to perform the character manipulations.
+    Then we return a string to use as our percentage output.
+    
+    Ex. if we receive 45.00, we would like just 45.
+        .. But if we receive 45.65, we want 45.65.
+        .. If we receive an int like 32, we want 32."""
+    if isinstance(numeric, float):
+        stripped = str(numeric).rstrip("0").rstrip(".")
+        
+        # Only integer digits were found
+        if stripped.isdigit():
+            return stripped # return string
+        # A . character was detected, thus there are decimal digits
+        else:
+            # Limit output to 2 decimal places
+            return f"{float(stripped):.2f}"
+    # Do nothing for ints
+    elif isinstance(numeric, int):
+        return str(numeric)
+    
 ################
 # Exercise 15: #
 ################
@@ -13,11 +37,11 @@ except ValueError:
 else:
     if seconds >= 60:
         minutes = seconds/60
-        print(f"\nMinutes: {minutes:.2f}")
+        print(f"\nMinutes: {strip_unwanted_zeros(minutes)}")
     if seconds >= 3600:
         hours = seconds/3600
-        print(f"Hours: {hours:.2f}")
+        print(f"Hours: {strip_unwanted_zeros(hours)}")
     if seconds >= 86400:
         days = seconds/86400
-        print(f"Days: {days.2f}")
+        print(f"Days: {strip_unwanted_zeros(days)}")
         
