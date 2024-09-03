e69ea1df87833931746d03485d5d77f9a4eaf984
Zowie Beha <113861530+1zzowiebeha@users.noreply.github.com>
2024-09-03 09:28:51 -0400

    Complete exercise 1

--- Week 3/Exercise1.py
+++ Week 3/Exercise1.py
@@ -9,3 +9,19 @@ try:
     number = int(input("Please enter a number:\n> "))
 except ValueError:
     print("Error: The value you entered was not a number.")
+else:
+    print("\n* Information about your number:")
+    if number < 0:
+        print("The number is Negative.")
+    elif number == 0:
+        print("The number is Zero.")
+    else:
+        print("The number is Positive.")
+    
+    # Divide by two and check if the remainder is 0
+    if number % 2 == 0:
+        print("The number is Even.")
+    # If a remainder exists, 2 can't perfectly fit into our number,
+    # .. and so it is odd.
+    else:
+        print("The number is Odd.")
