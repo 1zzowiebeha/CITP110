5a0ea8039a5232b6ebaa16481f41c8c5aebb7180
Zowie Beha <113861530+1zzowiebeha@users.noreply.github.com>
2024-09-03 10:15:52 -0400

    Progress towards exercise 16

--- Week 3/Exercise16.py
+++ Week 3/Exercise16.py
@@ -4,4 +4,23 @@
 # Exercise 16: #
 ################
 
-
+try:
+    year = int(input("Please enter a year:\n> "))
+except ValueError:
+    print("Error: The year you entered was not a number.")
+else:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            leap_year = True
+        else:
+            leap_year = False
+    elif year % 4 == 0:
+        leap_year = True
+    else:
+        leap_year = False
+        
+    if leap_year:
+        print(f"In {year}, February has 29 days.")
+    else:
+        print(f"In {year}, February has 28 days.")
+        
