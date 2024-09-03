f776e697a4dc818c733cd44d26df168d807f7625
Zowie Beha <113861530+1zzowiebeha@users.noreply.github.com>
2024-09-03 09:41:13 -0400

    Complete exercise 15

--- Week 3/Exercise15.py
+++ Week 3/Exercise15.py
@@ -1,5 +1,23 @@
 """Code for Exercise 15 from the textbook."""
 
+
 ################
 # Exercise 15: #
 ################
+print("Welcome. Welcome to the super second calculator.\n")
+
+try:
+    seconds = int(input("Please enter the number of seconds to calculate:\n> "))
+except ValueError:
+    print("Error: The input value must be a numeric.")
+else:
+    if seconds >= 60:
+        minutes = seconds/60
+        print(f"\nMinutes: {minutes:.2f}")
+    if seconds >= 3600:
+        hours = seconds/3600
+        print(f"Hours: {hours:.2f}")
+    if seconds >= 86400:
+        days = seconds/86400
+        print(f"Days: {days.2f}")
+        
