4debc786a7248c214c9ac685c0db9928e949fc05
Zowie Beha <113861530+1zzowiebeha@users.noreply.github.com>
2024-09-12 14:31:54 -0400

    Add another comment

--- Week 4/Exercise15.py
+++ Week 4/Exercise15.py
@@ -4,6 +4,8 @@
 for repetition_count in range(1):
     # Spaces between pound signs
     for space_count in range(6):
+        # A string multiplied by a number
+        # .. results in that string repeated.
         space_string = ' ' * space_count
         
         print(f"#{space_string}#")
