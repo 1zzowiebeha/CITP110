dd3e7e88712c342cf0a1f64f8322c66ea153ef27
Zowie Beha <113861530+1zzowiebeha@users.noreply.github.com>
2024-09-12 13:01:06 -0400

    Complete algorithm #1

---
+++ Week 4/algorithm_workbench.py
@@ -0,0 +1,16 @@
+"""Algorithm workbench assignment #1 from our textbook."""
+
+# type hint syntax
+product: float = 0
+
+while product < 100:
+    try:
+        numeric_input = float(input("Enter a number: "))
+    except ValueError:
+        print("Error: Entered value was not a number.")
+        # begin a new iteration
+        continue
+    
+    product += (numeric_input * 10)
+    
+print("Final product: ", product)
