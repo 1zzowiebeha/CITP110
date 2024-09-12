9dc6b9b6235590fa15e29d580ea588ac1c0607c3
Zowie Beha <113861530+1zzowiebeha@users.noreply.github.com>
2024-09-12 14:24:01 -0400

    Complete exercise 15

---
+++ Week 4/Exercise15.py
@@ -0,0 +1,9 @@
+"""Exercise #15 from our textbook."""
+
+# Just one repetition
+for repetition_count in range(1):
+    # Spaces between pound signs
+    for space_count in range(6):
+        space_string = ' ' * space_count
+        
+        print(f"#{space_string}#")
