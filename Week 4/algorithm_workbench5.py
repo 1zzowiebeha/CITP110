76d7e09b6efcc4c7fa5e37787cc47d63bd7f3a95
Zowie Beha <113861530+1zzowiebeha@users.noreply.github.com>
2024-09-12 13:04:31 -0400

    Complete algorithm 5

---
+++ Week 4/algorithm_workbench5.py
@@ -0,0 +1,14 @@
+"""Algorithm workbench assignment #5 from our textbook."""
+
+total: float = 0
+numerator: float = 0
+
+for denominator in range(30, 0, -1):
+    numerator += 1
+    
+    product = numerator / denominator
+    total += product
+    
+    print(f"{numerator}/{denominator} = {product}")
+    
+print("\nThe total is: ", total)
