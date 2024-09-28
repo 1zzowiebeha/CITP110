212f786c3f9fb23ad50974820fdfbecea9f9189e
Zowie Beha <113861530+1zzowiebeha@users.noreply.github.com>
2024-09-28 16:42:36 -0400

    Complete workbench 2

--- Week 6/algorithm_workbench2.py
+++ Week 6/algorithm_workbench2.py
@@ -1,1 +1,8 @@
 """Algorithm workbench #2 from Chapter 6 in our textbook."""
+
+# Note: your terminal working directory will
+#   .. determine where the output file is created!
+with open('number_lists.txt', 'w') as file_object:
+    # list comprehension:
+    numbers = [i for i in range(1,101)]
+    file_object.writelines(numbers)
