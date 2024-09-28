8267aa872118f6fe5c5f6fdbdcab3c42742a9806
Zowie Beha <113861530+1zzowiebeha@users.noreply.github.com>
2024-09-28 13:50:41 -0400

    Complete workbench 7

--- Week 6/algorithm_workbench7.py
+++ Week 6/algorithm_workbench7.py
@@ -1,13 +1,21 @@
 """Algorithm workbench #7 from Chapter 6 in our textbookl."""
 
+import os
+
+
 def remove_user_from_file(file_name: str, user: str) -> bool:
     """Remove the passed line of text from the file.
     Returns True if successful, False if no line was found."""
-    with open(file_name, "w+") as file_object:
+    # Read and write without truncating file upon read
+    with open(file_name, "r+", encoding="utf-8") as file_object:
         # Loads entire file into memory.
         # Not very efficient. See better methods here:
         # https://stackoverflow.com/questions/39086/search-and-replace-a-line-in-a-file-in-python
         lines = file_object.readlines()
+        
+        if not lines:
+            print('File is empty.')
+            return False
         
         line_to_remove = 0
         
@@ -27,6 +35,10 @@ def remove_user_from_file(file_name: str, user: str) -> bool:
         # A match was found in the file
         if len(lines) != line_to_remove:
             del lines[line_to_remove]
+            # Go back to the start of the file to write from byte 0
+            # instead of at the end of the file.
+            # (i'll have to read up on how this works)
+            file_object.seek(0)
             # Rewrite the file without the removed line
             file_object.writelines(lines)
             return True
@@ -35,8 +47,18 @@ def remove_user_from_file(file_name: str, user: str) -> bool:
         
         
 if __name__ == "__main__":
+    # If you run a python script from the commandline,
+    #   the current working directory will default to your current
+    #   directory. This can result in files being written/read
+    #   in the wrong folder.
+    # Here, we explicitly set where exactly we want to read/write the file.
+    # This is a common problem I've faced when working with files,
+    #   and I usually have to re-discover the solution every time.
+    # So I'll put this here and refer back when stuck :D 
+    file_path = os.path.join(os.getcwd(), 'Week 6/students.txt')
+    
     username = 'John Perz'
-    success = remove_user_from_file('students.txt', username)
+    success = remove_user_from_file(file_path, username)
     if success:
         print("Removed", username)
     else:
