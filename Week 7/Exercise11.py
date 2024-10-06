755c8a96b2fa6d3d04261d59c849c27b456a2493
Zowie Beha <113861530+1zzowiebeha@users.noreply.github.com>
2024-10-06 19:51:21 -0400

    Progress with exercise 11

--- Week 7/Exercise11.py
+++ Week 7/Exercise11.py
@@ -1,13 +1,15 @@
 """Exercise 11 from Chapter 7 of our textbook."""
 
-import numpy
-
 magic_square = [
     [7,8,9],
     [4,5,6],
     [1,2,3]
 ]
 
+# Take in magic square
+# ...
+
+# Sum all rows, columns, and the diagonal
 row_sums = [sum(row) for row in magic_square]
 
 col_sums = []
@@ -21,4 +23,18 @@ diagonal_sum += magic_square[0][0]
 diagonal_sum += magic_square[1][1]
 diagonal_sum += magic_square[2][2]
 
-# complete later on meh
+# Check equality
+a_value_not_equal = False
+for sum in row_sums:
+    if sum != diagonal_sum:
+        a_value_not_equal = True
+        
+for sum in col_sums:
+    if sum != diagonal_sum:
+        a_value_not_equal = True
+
+# Output
+if a_value_not_equal:
+    print("Magic square sums not equal!")
+else:
+    print("This is a magic square. All sums are equal! :)")
