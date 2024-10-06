6a57116c52863fd7cda54ab3f98a8b5563db41ed
Zowie Beha <113861530+1zzowiebeha@users.noreply.github.com>
2024-10-06 18:51:46 -0400

    Complete exercise 11

--- Week 7/Exercise11.py
+++ Week 7/Exercise11.py
@@ -1,1 +1,12 @@
 """Exercise 11 from Chapter 7 of our textbook."""
+
+import random
+
+lotto_ticket = []
+
+for iteration in range(7):
+    lotto_ticket.append(random.randint(0,9))
+    
+print("\nYour ticket numbers are:")
+for lucky_number in lotto_ticket:
+    print(lucky_number)
--- Week 7/Exercise2.py
+++ Week 7/Exercise2.py
@@ -1,1 +1,2 @@
 """Exercise 2 from Chapter 7 of our textbook."""
+
