b047c133c02f44a07b4b052757db572dd12a5c34
Zowie Beha <113861530+1zzowiebeha@users.noreply.github.com>
2024-10-06 18:45:53 -0400

    Progress with exercise 14

--- Week 7/Exercise 14/Exercise14.py
+++ Week 7/Exercise 14/Exercise14.py
@@ -1,3 +1,60 @@
-"""Exercise 14 from Chapter 7 of our textbook."""
+"""Exercise 14 from Chapter 7 of our textbook.
+Be sure to use a virtual environment and install
+matplotlib and numpy via pip if you use a desktop installation of python.
+"""
 
-import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+def get_csv_columns(csv_path: str) -> list:
+    """Retrieve csv file column names."""
+    with open(csv_path, 'r') as file_object:
+        for line in file_object:
+            columns = line.split(',')
+            # Strip whitespace
+            for index, column in enumerate(columns):
+                columns[index] = column.strip()
+
+            # exit after first line (since we only need that one)
+            return columns
+    
+    
+def get_csv_values(csv_path: str) -> list:
+    """Retrieve values in csv file.
+    Return a multi-dimensional array to deliminate value rows."""
+    with open(csv_path, 'r') as file_object:
+        values = []
+        
+        first_line_skipped = False
+        for line in file_object:
+            # Skip first column definition line
+            if not first_line_skipped:
+                first_line_skipped = True
+                continue
+            
+            line_values = line.split(',')
+            # Strip whitespace
+            for index, value in enumerate(line_values):
+                line_values[index] = value.strip()
+
+            values.append(line_values)
+            
+        return values
+
+
+if __name__ == "__main__":
+    csv_file_path = os.path.join(os.getcwd(), 'expenses.csv')
+    
+    categories = get_csv_columns('expenses.csv')
+    # Get first row of multi-dimentional array
+    values = get_csv_values('expenses.csv')[0]
+    
+    print()
+    print(categories, values)
+    
+    # fig, ax = plt.subplots(figsize=(5, 2.7), layout='constrained')
+
+    # ax.bar(categories, values)
+    
+    # yay
