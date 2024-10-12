6b3372711e9b5e80346738fa66c5f552a5bc4926
Zowie Beha <113861530+1zzowiebeha@users.noreply.github.com>
2024-10-12 16:42:15 -0400

    Complete exercise 1

--- Week 8/Exercise1.py
+++ Week 8/Exercise1.py
@@ -1,1 +1,14 @@
 """Exercise 1 from Chapter 8 of our textbook."""
+
+if __name__ == "__main__":
+    # Separate previous terminal commands from output
+    print()
+
+    fname = input("Enter your first name: ")
+    lname = input("Enter your last name: ")
+
+    print("\nHere are your initials, your address name, and your username:")
+
+    print(fname[0].upper() + '.' + lname[0].upper())
+    print(fname.title() + ' ' + lname.upper())
+    print(fname[0].lower() + lname.lower())
